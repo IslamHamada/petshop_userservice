c235e8ade985b4c1c698dfdf254987b2fc4d934c
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 22:44:10 +0100

    add Builder annotation, change the id

--- src/main/java/com/islamhamada/UserService/entity/User.java
+++ src/main/java/com/islamhamada/UserService/entity/User.java
@@ -3,14 +3,19 @@ package com.islamhamada.UserService.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
+@Builder
 public class User {
     @Id
     @GeneratedValue
-    private long id;
+    private long numId;
 
-    String username;
+    private String id;
+
+    private String username;
+    private String email;
 }
