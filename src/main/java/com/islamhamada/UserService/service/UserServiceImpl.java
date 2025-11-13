da13809e8c98a44e8d99e8ac1e837449ffe9d7a6
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 22:51:24 +0100

    An implementation of the storeUser method

--- src/main/java/com/islamhamada/UserService/service/UserServiceImpl.java
+++ src/main/java/com/islamhamada/UserService/service/UserServiceImpl.java
@@ -1,7 +1,32 @@
 package com.islamhamada.UserService.service;
 
+import com.islamhamada.UserService.entity.User;
+import com.islamhamada.UserService.model.StoreUserRequest;
+import com.islamhamada.UserService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public long storeUser(StoreUserRequest request) {
+        Optional<User> userOptional = userRepository.findByAuth0Id(request.getAuth0_id());
+        if(userOptional.isPresent()){
+            return userOptional.get().getId();
+        } else {
+            User user = User.builder()
+                    .auth0Id(request.getAuth0_id())
+                    .username(request.getEmail())
+                    .email(request.getEmail())
+                    .build();
+            user = userRepository.save(user);
+            return user.getId();
+        }
+    }
 }
