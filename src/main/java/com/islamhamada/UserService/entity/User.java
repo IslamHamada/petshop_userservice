a36a9723490e0b652a7f67a8ae2b627b73e244d7
IslamHamada <Islamhamada222@gmail.com>
2025-11-14 14:30:37 +0100

    add needed lombok annotations

--- src/main/java/com/islamhamada/UserService/entity/User.java
+++ src/main/java/com/islamhamada/UserService/entity/User.java
@@ -3,12 +3,16 @@ package com.islamhamada.UserService.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue
