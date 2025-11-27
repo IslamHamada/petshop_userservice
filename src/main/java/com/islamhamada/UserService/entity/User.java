6f1b53e601a4a62154a6b62dd140efdc5da1c4e8
IslamHamada <Islamhamada222@gmail.com>
2025-11-27 18:22:53 +0100

    extend the user entity with more vars

--- src/main/java/com/islamhamada/UserService/entity/User.java
+++ src/main/java/com/islamhamada/UserService/entity/User.java
@@ -1,5 +1,6 @@
 package com.islamhamada.UserService.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -7,6 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Entity
 @Data
@@ -18,8 +21,34 @@ public class User {
     @GeneratedValue
     private long id;
 
+    @Column(unique = true)
     private String auth0Id;
 
     private String username;
+
+    @Column(unique = true)
     private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String street;
+
+    @Column(name = "house_number")
+    private String houseNumber;
+
+    private String city;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    private String country;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
 }
