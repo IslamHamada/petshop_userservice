edc701f8253b5de3dda6961f7354c9fdf9cfd56c
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:37:09 +0100

    add multiple validation annotations

--- src/main/java/com/islamhamada/UserService/model/StoreUserRequest.java
+++ src/main/java/com/islamhamada/UserService/model/StoreUserRequest.java
@@ -1,10 +1,15 @@
 package com.islamhamada.UserService.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class StoreUserRequest {
+    @NotBlank
     private String auth0_id;
+    @NotBlank
     private String username;
+    @NotBlank @Email
     private String email;
 }
