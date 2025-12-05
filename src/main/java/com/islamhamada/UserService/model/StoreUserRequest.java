02ec72b030a4eab6afc58e22901a4e46b82b8203
IslamHamada <Islamhamada222@gmail.com>
2025-12-05 23:14:30 +0100

    add @Builder @AllArgsConstructor @NoArgsConstructor to make testing easier

--- src/main/java/com/islamhamada/UserService/model/StoreUserRequest.java
+++ src/main/java/com/islamhamada/UserService/model/StoreUserRequest.java
@@ -2,9 +2,15 @@ package com.islamhamada.UserService.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StoreUserRequest {
     @NotBlank
     private String auth0_id;
