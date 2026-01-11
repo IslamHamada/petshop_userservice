21e036a70995c09e5f1f7e9a9558b0ccda1d97c0
IslamHamada <islamhamada222@gmail.com>
2026-01-11 10:34:36 +0100

    add getUsername service declaration + implementation + get endpoint

--- src/main/java/com/islamhamada/UserService/service/UserService.java
+++ src/main/java/com/islamhamada/UserService/service/UserService.java
@@ -3,9 +3,11 @@ package com.islamhamada.UserService.service;
 import com.islamhamada.UserService.model.StoreUserRequest;
 import com.islamhamada.UserService.model.UpdateUserRequest;
 import com.islamhamada.petshop.contracts.dto.UserDTO;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public interface UserService {
     long storeUser(StoreUserRequest request);
     UserDTO getUser(long userId);
     UserDTO updateUser(long user_id, UpdateUserRequest request);
+    String getUsername(@PositiveOrZero long userId);
 }
