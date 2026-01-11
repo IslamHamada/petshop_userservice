7814b49a80e16f42e198349ab93c688e68dd4bed
IslamHamada <islamhamada222@gmail.com>
2026-01-11 12:47:52 +0100

    separate paths of rest endpoints depending on which needs authorization

--- src/main/java/com/islamhamada/UserService/controller/UserController.java
+++ src/main/java/com/islamhamada/UserService/controller/UserController.java
@@ -21,25 +21,25 @@ public class UserController {
     UserService userService;
 
     @PreAuthorize("hasAnyRole('Customer')")
-    @PostMapping
+    @PostMapping("/protected")
     public ResponseEntity<Long> storeUser(@Valid @RequestBody StoreUserRequest request){
         return new ResponseEntity<>(userService.storeUser(request), HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('Customer')")
-    @GetMapping("/{user_id}")
+    @GetMapping("/protected/{user_id}")
     public ResponseEntity<UserDTO> getUser(@PositiveOrZero @PathVariable long user_id){
         return new ResponseEntity<>(userService.getUser(user_id), HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('Customer')")
-    @PutMapping("/{user_id}")
+    @PutMapping("/protected/{user_id}")
     public ResponseEntity<UserDTO> updateUser(@PositiveOrZero @PathVariable long user_id, @Valid @RequestBody UpdateUserRequest request){
         UserDTO user = userService.updateUser(user_id, request);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @GetMapping("/username/{userId}")
+    @GetMapping("/public/username/{userId}")
     public ResponseEntity<String> getUsername(@PositiveOrZero @PathVariable long userId){
         return new ResponseEntity<>(userService.getUsername(userId), HttpStatus.OK);
     }
