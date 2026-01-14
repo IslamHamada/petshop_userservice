8a88129e080d821035f4043fd689187aacc3f440
IslamHamada <islamhamada222@gmail.com>
2026-01-14 18:18:06 +0100

    optimize imports + add missing import

--- src/main/java/com/islamhamada/UserService/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/islamhamada/UserService/exception/RestResponseEntityExceptionHandler.java
@@ -1,15 +1,11 @@
 package com.islamhamada.UserService.exception;
 
 import com.islamhamada.petshop.contracts.model.RestExceptionResponse;
-import org.springframework.http.HttpHeaders;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
+import org.springframework.http.*;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Log4j2
 @ControllerAdvice
