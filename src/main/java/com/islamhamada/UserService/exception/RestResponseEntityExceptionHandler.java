dcd609087fbf7ba19bcc48cad06fc12f933aab5c
IslamHamada <islamhamada222@gmail.com>
2026-01-14 10:08:08 +0100

    override handleMethodArgumentNotValid exception

--- src/main/java/com/islamhamada/UserService/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/islamhamada/UserService/exception/RestResponseEntityExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.islamhamada.UserService.exception;
 
 import com.islamhamada.petshop.contracts.model.RestExceptionResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -15,5 +20,13 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
                 .error_code(exception.getError_code())
                 .error_message(exception.getMessage())
                 .build(), exception.getHttpStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object>handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return new ResponseEntity<>(RestExceptionResponse.builder()
+                .error_code(exception.getDetailMessageCode())
+                .error_message(exception.getMessage())
+                .build(), status);
     }
 }
